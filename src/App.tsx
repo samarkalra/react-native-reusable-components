7f2c55c4b95966ca2f492f14de6f8ed3093c1f5b
samarkalra <skalra@ex2india.com>
2022-03-04 17:01:04 +0530

    Clean App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,115 +1,49 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
+import {ImageSourcePropType, View} from 'react-native';
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {AppTextInput} from './components';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section: React.FC<{
-  title: string;
-}> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+const EMAIL_ICON: ImageSourcePropType = {
+  uri: 'https://img.icons8.com/material-outlined/24/000000/mail.png',
+};
+const PASSWORD_ICON: ImageSourcePropType = {
+  uri: 'https://img.icons8.com/ios-glyphs/30/000000/password--v1.png',
 };
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <AppTextInput
+        placeholder="Email"
+        iconLeft={EMAIL_ICON}
+        containerStyle={{width: '80%', alignSelf: 'center', borderWidth: 0}}
+        inputContainerStyle={{
+          borderWidth: 1,
+          borderColor: '#ccc',
+          borderRadius: 12,
+        }}
+        multiline
+        style={{borderWidth: 0}}
+      />
+
+      <AppTextInput
+        placeholder="Password"
+        iconLeft={PASSWORD_ICON}
+        secureTextEntry={true}
+        containerStyle={{
+          width: '80%',
+          alignSelf: 'center',
+          marginTop: 12,
+          borderWidth: 0,
+        }}
+        inputContainerStyle={{
+          borderWidth: 1,
+          borderColor: '#ccc',
+          borderRadius: 12,
+        }}
+        style={{borderWidth: 0}}
+      />
+    </View>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
